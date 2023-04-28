9c57c1b1a93e19adc1728b759366fe454a77dbb7
Dipyaman Das <122435063+dipyaman-das@users.noreply.github.com>
2023-04-29 02:08:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("hello");
+var typed = new Typed('.element', {
+    strings: ["Frontend Developer.", "Good Man."],
+    typeSpeed: 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+  });
